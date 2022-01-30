afdd0420117e7b00bc485734cb8cc54b5d2e81e5
GB-Navarro <devnavarro06@gmail.com>
2022-01-29 23:00:16 -0300

    Versão com os problemas do JS corrigidos

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,34 +1,153 @@
-/*alert(document.querySelector(".box_opcoes > .greenborder"))*/
+/*-------------------------------------------------------------------------------*/
 
-function colocarBordanasComidas(){
-    const elementoComida = document.querySelector(".opcoes_comida");
-    if(document.querySelector(".comidas > .greenborder") != null){
-        elementoComida.classList.remove("greenborder");
-
-    } else if(document.querySelector(".comidas > .greenborder") === null){
-        elementoComida.classList.add("greenborder");
+function colocarBordanaOpcao1dasComidas(){
+    const elemento = document.querySelector(".comidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
     }
+    const opcao1Comida = document.querySelector(".opcao1Comida");
+    opcao1Comida.classList.add("greenborder");
+}
+function colocarBordanaOpcao2dasComidas(){
+    const elemento = document.querySelector(".comidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao2Comida = document.querySelector(".opcao2Comida");
+    opcao2Comida.classList.add("greenborder");
+}
+function colocarBordanaOpcao3dasComidas(){
+    const elemento = document.querySelector(".comidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao3Comida = document.querySelector(".opcao3Comida");
+    opcao3Comida.classList.add("greenborder");
+}
+function colocarBordanaOpcao4dasComidas(){
+    const elemento = document.querySelector(".comidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao4Comida = document.querySelector(".opcao4Comida");
+    opcao4Comida.classList.add("greenborder");
+}
+function colocarBordanaOpcao5dasComidas(){
+    const elemento = document.querySelector(".comidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao5Comida = document.querySelector(".opcao5Comida");
+    opcao5Comida.classList.add("greenborder");
+}
+function colocarBordanaOpcao6dasComidas(){
+    const elemento = document.querySelector(".comidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao6Comida = document.querySelector(".opcao6Comida");
+    opcao6Comida.classList.add("greenborder");
+}
+/*-------------------------------------------------------------------------------*/
+
+function colocarBordanaOpcao1dasBebidas(){
+    const elemento = document.querySelector(".bebidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao1Bebida = document.querySelector(".opcao1Bebida");
+    opcao1Bebida.classList.add("greenborder");
+}
+function colocarBordanaOpcao2dasBebidas(){
+    const elemento = document.querySelector(".bebidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao2Bebida = document.querySelector(".opcao2Bebida");
+    opcao2Bebida.classList.add("greenborder");
+}
+function colocarBordanaOpcao3dasBebidas(){
+    const elemento = document.querySelector(".bebidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao3Bebida = document.querySelector(".opcao3Bebida");
+    opcao3Bebida.classList.add("greenborder");
+}
+function colocarBordanaOpcao4dasBebidas(){
+    const elemento = document.querySelector(".bebidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao4Bebida = document.querySelector(".opcao4Bebida");
+    opcao4Bebida.classList.add("greenborder");
+}
+function colocarBordanaOpcao5dasBebidas(){
+    const elemento = document.querySelector(".bebidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao5Bebida = document.querySelector(".opcao5Bebida");
+    opcao5Bebida.classList.add("greenborder");
+}
+function colocarBordanaOpcao6dasBebidas(){
+    const elemento = document.querySelector(".bebidas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao6Bebida = document.querySelector(".opcao6Bebida");
+    opcao6Bebida.classList.add("greenborder");
+}
+/*-------------------------------------------------------------------------------*/
+
+function colocarBordanaOpcao1dasSobremesas(){
+    const elemento = document.querySelector(".sobremesas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao1Sobremesa = document.querySelector(".opcao1Sobremesa");
+    opcao1Sobremesa.classList.add("greenborder");
+}
+function colocarBordanaOpcao2dasSobremesas(){
+    const elemento = document.querySelector(".sobremesas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao2Sobremesa = document.querySelector(".opcao2Sobremesa");
+    opcao2Sobremesa.classList.add("greenborder");
+}
+function colocarBordanaOpcao3dasSobremesas(){
+    const elemento = document.querySelector(".sobremesas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao3Sobremesa = document.querySelector(".opcao3Sobremesa");
+    opcao3Sobremesa.classList.add("greenborder");
+}
+function colocarBordanaOpcao4dasSobremesas(){
+    const elemento = document.querySelector(".sobremesas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao4Sobremesa = document.querySelector(".opcao4Sobremesa");
+    opcao4Sobremesa.classList.add("greenborder");
+}
+function colocarBordanaOpcao5dasSobremesas(){
+    const elemento = document.querySelector(".sobremesas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao5Sobremesa = document.querySelector(".opcao5Sobremesa");
+    opcao5Sobremesa.classList.add("greenborder");
+}
+function colocarBordanaOpcao6dasSobremesas(){
+    const elemento = document.querySelector(".sobremesas > .greenborder");
+    if(elemento != null){
+        elemento.classList.remove("greenborder");
+    }
+    const opcao6Sobremesa = document.querySelector(".opcao6Sobremesa");
+    opcao6Sobremesa.classList.add("greenborder");
 }
 
-function colocarBordanasBebidas(){
-    const elementoBebida = document.querySelector(".opcoes_bebida");
-    if(document.querySelector(".bebidas > .greenborder") != null){
-        elementoBebida.classList.remove("greenborder");
+/*-------------------------------------------------------------------------------*/
 
-    } else if(document.querySelector(".bebidas > .greenborder") === null){
-        elementoBebida.classList.add("greenborder");
-    }
-}
-
-function colocarBordanasSobremesas(){
-    const elementoSobremesa = document.querySelector(".opcoes_sobremesa");
-    if(document.querySelector(".sobremesas > .greenborder") != null){
-        elementoSobremesa.classList.remove("greenborder");
-
-    } else if(document.querySelector(".sobremesas > .greenborder") === null){
-        elementoSobremesa.classList.add("greenborder");
-    }
-}
-/*
-if()
-*/
